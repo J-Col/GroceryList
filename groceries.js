21a91df128f043c98619afdcdfcd31ad4113d582
Joshua Colclazier <jcolclazier@mail.csuchico.edu>
2016-11-30 16:12:45 -0800

    Final

--- groceries.js
+++ groceries.js
@@ -1,3 +1,71 @@
+window.onload = loadCookieList;
+
+var myList = [];
+
+function loadCookieList()
+{
+  var cookie = getCookie("groceryList");
+  if (cookie == "") return;
+  var arrayCookie = cookie.split("|");
+  for (var i = 0; i < arrayCookie.length; i++)
+  {
+    displayItem(arrayCookie[i]);
+  }
+}
+
+function addItem() {
+  var input = document.getElementById("newItem");
+  displayItem(input.value);
+  input.value = "";
+}
+
+function displayItem(userInput)
+{
+  if (myList.indexOf(userInput) != -1)
+  return;
+  myList.push(userInput);
+  console.log(myList);
+  var list = document.getElementById("listDisplay");
+  var item = document.createElement("li");
+  var btnClose = document.createElement("button");
+  btnClose.classList.add("btn");
+  btnClose.classList.add("btn-danger");
+  btnClose.classList.add("btn-xs");
+  var iconClose = document.createElement("span");
+  iconClose.classList.add("glyphicon");
+  iconClose.classList.add("glyphicon-remove");
+  btnClose.appendChild(iconClose);
+  btnClose.addEventListener("click", removeParentListItem);
+  item.appendChild(btnClose);
+  var itemName = document.createTextNode(userInput);
+  item.appendChild(itemName);
+  list.appendChild(item);
+}
+
+function saveList()
+{
+  var listString = myList.join();
+  setCookie("groceryList", listString, 2)
+}
+
+function clearList()
+{
+  var listD = document.getElementById("listDisplay");
+  while (listD.firstChild) {
+    listD.removeChild(listD.firstChild);
+  myList = [];
+  }
+}
+
+function removeParentListItem() {
+  var mom = this.parentNode;
+  var itemRemove = mom.firstChild;
+  var itemIndex = myList.indexOf(itemRemove);
+  myList.splice(itemIndex,1);
+  console.log(myList);
+  var grandma = mom.parentNode;
+  grandma.removeChild(mom);
+}
 
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
 function setCookie(cname, cvalue, exdays) {
@@ -20,51 +88,4 @@ function getCookie(cname) {
         }
     }
     return "";
-}
-var myList = [];
-function saveList()
-{
-  var listString = myList.join();
-  setCookie("groceryList", listString, 2)
-}
-function clearList()
-{
-  var listD = document.getElementById("listDisplay");
-  while (listD.firstChild) {
-    listD.removeChild(listD.firstChild);
-  myList = [];
-  }
-}
-function addItem() {
-  var input = document.getElementById("newItem");
-  if (myList.indexOf(input.value) != -1)
-  return;
-  myList.push(input.value);
-  console.log(myList);
-  var list = document.getElementById("listDisplay");
-  var item = document.createElement("li");
-  var btnClose = document.createElement("button");
-  btnClose.classList.add("btn");
-  btnClose.classList.add("btn-danger");
-  btnClose.classList.add("btn-xs");
-  var iconClose = document.createElement("span");
-  iconClose.classList.add("glyphicon");
-  iconClose.classList.add("glyphicon-remove");
-  btnClose.appendChild(iconClose);
-  btnClose.addEventListener("click", removeParentListItem);
-  item.appendChild(btnClose);
-  var itemName = document.createTextNode(input.value);
-  item.appendChild(itemName);
-  list.appendChild(item);
-  input.value = "";
-}
-
-function removeParentListItem() {
-  var mom = this.parentNode;
-  var itemRemove = mom.firstChild;
-  var itemIndex = myList.indexOf(itemRemove);
-  myList.splice(itemIndex,1);
-  console.log(myList);
-  var grandma = mom.parentNode;
-  grandma.removeChild(mom);
 }
